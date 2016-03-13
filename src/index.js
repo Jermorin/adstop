a90c476417e96c8d0a349436e5d539902dc2c888
Jérémy Morin <hi@jermor.in>
2016-03-13 17:05:35 +0100

    feat(version) v0.0.3

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import program from 'commander';
 import request from 'request';
 import fs from 'fs';
+import chalk from 'chalk';
+
+const WINDOWS = process.platform === 'win32';
+
+const HOSTS = WINDOWS
+  ? 'C:/Windows/System32/drivers/etc/hosts'
+  : '/etc/hosts';
 
 const urlHostFileTpl = 'https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts';
 
@@ -10,21 +17,26 @@ const apply = () => {
       .get(urlHostFileTpl)
       .on('error', (err) => reject(err))
       .on('response', response => resolve(response.statusCode))
-      .pipe(fs.createWriteStream('/etc/hosts'));
+      .pipe(fs.createWriteStream(HOSTS));
   });
 };
 
-program.version('0.0.1');
+program.version('0.0.3');
 
 program
   .command('apply')
   .description('Apply adstop hostfile')
   .action(() => {
-    apply().then(response => {
-      if (response === 200){
-        console.log('> success, reboot ur computer !')
-      }
-    }).catch(e => console.log(err));
+    var uid = parseInt(process.env.SUDO_UID);
+    if (uid) {
+      apply().then(response => {
+        if (response === 200) {
+          console.log(chalk.green('☭ Success, reboot ur computer !'));
+        }
+      }).catch(err => console.log(chalk.red(err)));
+    } else {
+      console.log(chalk.red(`Program can\'t access on ${HOSTS} without sudo permission`));
+    }
   });
 
 program.parse(process.argv);
