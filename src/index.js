080f3881de679144ffe909add9218c00cf2d465d
Jérémy Morin <hi@jermor.in>
2016-03-13 18:23:49 +0100

    feat(version) v0.1.2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-#! /usr/bin/env node
-
 import program from 'commander';
 import request from 'request';
 import fs from 'fs';
@@ -23,7 +21,7 @@ const apply = () => {
   });
 };
 
-program.version('0.1.1');
+program.version('0.1.2');
 
 program
   .command('apply')
